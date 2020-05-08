ae1661bd15693cb55424a4d7722ce6d2a0e7da7e
Michael Mamuric <michael.mamuric@edu.sait.ca>
2020-05-08 12:51:47 -0600

    Data sanitation using validator module: playground

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { 
     useNewUrlParser: true,
@@ -7,16 +8,60 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     firstName: {
-        type: String        
+        type: String,
+        required: true,
+        trim: true        
     },
     lastName: {
-        type: String
-    }
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value))
+                throw new Error('Email is invalid')   
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        // Custom validator
+        validate(value) {
+            if(value < 0)
+                throw new Error('Age cannot be a negative number')
+        }
+    } 
 })
 
+// Valid user without age and e-mail
 const user = new User({
     firstName: 'Michael',
-    lastName: false    
+    lastName: 'Mamuric'
+})
+
+// Invalid user with invalid age
+const user2 = new User({
+    firstName: 'Anne',
+    lastName: 'Annerson',
+    age: -1    
+})
+
+// Invalid user with invalid email
+const user3 = new User({
+    firstName: 'Peter',
+    lastName: 'Peterson',
+    email: 'peter@'
+})
+
+// Valid user with an uppercase e-mail and no age provided
+const user4 = new User({
+    firstName: 'John',
+    lastName: 'Johnsson',
+    email: 'JOHN@EMAIL.COM'
 })
 
 user.save().then(() => {
@@ -25,18 +70,36 @@ user.save().then(() => {
     console.log('Error', error)
 })
 
-const Task = mongoose.model('Task', {
-    description: {type: String},
-    completed: {type: Boolean}    
-})
-
-const newTask = new Task({
-    description: 'Clean the house',
-    completed: true
-})
-
-newTask.save().then(() => {
-    console.log(newTask)
+user2.save().then(() => {
+    console.log(user)
 }).catch((error) => {
-    console.log(error)
+    console.log('Error', error)
 })
+
+user3.save().then(() => {
+    console.log(user)
+}).catch((error) => {
+    console.log('Error', error)
+})
+
+user4.save().then(() => {
+    console.log(user)
+}).catch((error) => {
+    console.log('Error', error)
+})
+
+// const Task = mongoose.model('Task', {
+//     description: {type: String},
+//     completed: {type: Boolean}    
+// })
+
+// const newTask = new Task({
+//     description: 'Clean the house',
+//     completed: true
+// })
+
+// newTask.save().then(() => {
+//     console.log(newTask)
+// }).catch((error) => {
+//     console.log(error)
+// })
